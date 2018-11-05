708dcd8fb5fc422e52357277f95b0d08a80c72db
Caroline Berg <carolineeberg@gmail.com>
2018-11-05 00:10:02 +0000

    Methods Updates

--- lib/getaway_deals/cli.rb
+++ lib/getaway_deals/cli.rb
@@ -1,28 +1,30 @@
 class GetawayDeals::CLI 
   
   def call 
-    puts "Today's Getaway Deals:"
-    list_deals
+    getaway_packages
     menu
     goodbye
   end
   
-  def list_deals 
+  def getaway_packages 
+    puts "Today's Getaway Specials:"
     @getaways = GetawayDeals::Getaway.all
+    @getaways.each.with_index(1) do |getaway, i| 
+      puts "#{i}. #{getaway.name} - #{getaway.price} - #{getaway.rating}" 
+    end
   end 
   
   def menu 
     input = nil 
     while input != "exit"
-     puts "Enter the number of the deal you'd like more info on or type 'list' to see the deals again or type 'exit':"
+     puts "Enter the number of the deal you'd like more info on or type 'getaway' to see the deals again or type 'exit':"
     input = gets.strip.downcase 
-      case input 
-      when "1"
-        puts "More info on 1!"
-      when "2" 
-        puts "More info on 2!"
-      when "list"
-        list_deals
+      
+      if input.to_i > 0 
+        the_getaway = @getaways[input.to_i-1]
+        puts "#{the_getaway.name} - #{the_getaway.price} - #{the_getaway.rating}"
+      elsif "getaway"
+        getaway_packages
       else 
         puts "Not sure what you want. Please type 'list' or 'exit'."
       end
--- lib/getaway_deals/getaway.rb
+++ lib/getaway_deals/getaway.rb
@@ -2,21 +2,16 @@ class GetawayDeals::Getaway
   attr_accessor :name, :price, :rating, :url
   
   def self.all 
-    #return a bunch on instances of getaway deals
-       #HereDoc with squiggly...
-   # puts <<-DOC.gsub /^\s*/, ''
-    #1. Deal One - price - still available!
-    #2. Deal Two - price - still available!
-    #3. Etc
-    #DOC
-    getaway_1 = self.new 
-    getaway_1.name = "Dubai Vacation"
-    getaway_1.price = "$999"
-    getaway_1.rating = "4/5 Stars" 
-    getaway_1.url = "https://www.groupon.com/deals/ga-pacific-holidays-dubai-26"
-    
-    [getaway_1]
-    #return array of data/getaways 
+    self.scrape_getaways
   end
   
+  def scrape_getaways 
+    deals = [] 
+    deals << self.scrape_groupon 
+    deals 
+  end 
+  
+  def self.scrape_groupon 
+    doc = Nokogiri::HTML(open("https://www.groupon.com/getaways"))
+  end
 end
