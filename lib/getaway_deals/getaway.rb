3717955f256d29570bef2c33f6e3135cbe6cda33
Caroline Berg <carolineeberg@gmail.com>
2018-11-06 02:24:18 +0000

    Scrape Updates

--- lib/getaway_deals/getaway.rb
+++ lib/getaway_deals/getaway.rb
@@ -1,5 +1,7 @@
+require 'pry'
+
 class GetawayDeals::Getaway
-  attr_accessor :location, :price, :getaway_url, :rating, :description, :buy
+  attr_accessor :title, :price, :getaway_url, :rating, :description, :buy
   @@all = []
   
   def initialize(getaway_hash)
@@ -16,16 +18,29 @@ class GetawayDeals::Getaway
     groupon_getaways = [] 
     
      page.css(".card-ui").each do |getaway| 
-        location = getaway.css(".cui-udc-title-with-subtitle").text.gsub("\n", "").strip
+        title = getaway.css(".cui-udc-title-with-subtitle").text.gsub("\n", "").strip
         price = getaway.css(".cui-price-discount").text.gsub("From ", "").gsub("/nt", "")
         getaway_url = getaway.css("a").attribute("href").value 
         
-        getaway_info = {:location => location, 
+        getaway_info = {:title => title, 
                         :price => price,
                         :getaway_url => getaway_url} 
         groupon_getaways << getaway_info 
       end 
     groupon_getaways 
   end 
+  
+  def self.scrape_getaway_page
+    getaway_page = Nokogiri::HTML(open("https://www.groupon.com/deals/ga-gate-1-travel-reykjavik-27"))
+    
+    deal_title = getaway_page.css("h1.deal-page-title").text.gsub("\n", "").strip
+    deal_price = getaway_page.css(".price").text 
+    
+    
+     
+     binding.pry
+    
+    
+  end
 
 end
